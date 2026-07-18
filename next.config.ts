7ff8be18f794a72e0a631f229aa3241bf8bb112d
Aniruddh Nalli <167133304+aniruddhnalli@users.noreply.github.com>
2026-07-18 14:02:28 +0530

    Enhance Next.js configuration with image optimization and TypeScript settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    dirs: ["src"],
+  },
+  typescript: {
+    tsconfigPath: "./tsconfig.json",
+  },
 };
 
 export default nextConfig;
